69200b62d0a8a3ce9f370bdbb07253a8aafe6209
Khushi Bhoj <khushikavya11@gmail.com>
2021-10-06 00:57:47 +0530

    pre-heroku

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/ps5'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/ps5'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://biawoyxnezotex:63f86296727f5f238ab5044c55e588d0fb0352ee80cb7b1e2847e9bb336a28a4@ec2-18-214-214-252.compute-1.amazonaws.com:5432/d2ordsesd5srav'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 class contactus(db.Model):
@@ -11,6 +12,12 @@ class contactus(db.Model):
     name = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(40), nullable=False)
     feedback = db.Column(db.String(500), nullable=False)
+
+def __init__(self, id, name, email, comments):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.comments = comments
 
 @app.route("/")
 def home():
